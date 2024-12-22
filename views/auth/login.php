25c8899c35a6d6d2f879e36299dc1b6009670f31
mcamilamp <camimerpa@gmail.com>
2024-12-21 20:01:35 -0500

    Add SCSS files for authentication and form components of login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,27 @@
-<h1>Login</h1>
+<div class="contenedor login">
+    <h1 class="uptask">UpTask</h1>
+    <p class="tagline">Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form action="/" class="formulario" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Tu Email">
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" name="email" id="password" placeholder="Tu Password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? Crea una</a>
+            <a href="/olvide">¿Olvidaste tu password?</a>
+        </div>
+    </div> <!--.contenedor-sm-->
+</div>
